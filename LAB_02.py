511906aa5bc67dfed65ca4ca474209a02b8bd57d
Blazej <nitka.blazej@gmail.com>
2020-12-07 23:10:32 +0100

    Refactor code

--- LAB_02.py
+++ LAB_02.py
@@ -10,46 +10,69 @@ from sklearn.cluster import DBSCAN
 colors = ['orange', 'brown', 'red', 'green', 'blue', 'black', 'magenta', 'yellow', 'gray', 'crimson']
 markers = ['o', 's', 'p', 'P', '*', 'h', 'D', 'd', '+', '_']
 
+
+def get_clusters_number():
+    valid_value = 0
+    user_number = 5
+    while valid_value == 0:
+        user_number = int(input("Set number of clusters (from 2 to 10): "))
+        if 1 < user_number < 11:
+            valid_value = 1
+        else:
+            print("\nValue must be between [2;10]")
+    return user_number
+
+
+def show_kmeans_chart(initialize_method, name):
+    # Using KMeans
+    kmeans = KMeans(n_clusters=n_clusters, init=initialize_method)
+    y_predicted = kmeans.fit_predict(transactions[["W0", "Normalized 0"]])
+    transactions["cluster"] = y_predicted
+    transactions.head()
+
+    # Draw each cluster
+    for index in range(n_clusters):
+        filtered_items = transactions[transactions.cluster == index]
+        plt.scatter(filtered_items["W0"], filtered_items["Normalized 0"], color=colors[index], marker=markers[index],
+                    label="Skupienie " + str(index + 1))
+
+    # Draw center points and show legend
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], color='purple', marker='X',
+                label="Centroidy")
+    plt.title(name + ' - wykres z podziałem na klastry: %d' % n_clusters)
+    plt.legend()
+    plt.show()
+
+
+def show_dbscan_chart():
+    # DBSCAN - Create matrix from columns with analyzed data and use DBSCAN
+    matrix = np.column_stack((transactions["W0"], transactions["Normalized 0"]))
+    model = DBSCAN(eps=0.25, min_samples=400).fit(matrix)
+    plt.scatter(matrix[:, 0], matrix[:, 1], c=model.labels_)
+    plt.title('DBSCAN - wykres')
+    plt.show()
+
+
+def scale_week_column():
+    # Scale values to 0-1
+    scaler = MinMaxScaler()
+    scaler.fit(transactions[['W0']])
+    transactions['W0'] = scaler.transform(transactions[['W0']])
+
+
 # Read CSV file
 transactions = pd.read_csv("sources/Sales_Transactions_Dataset_Weekly.csv")
 
-# Scale values to 0-1
-scaler = MinMaxScaler()
-scaler.fit(transactions[['W0']])
-transactions['W0'] = scaler.transform(transactions[['W0']])
+# User can set clusters number
+n_clusters = get_clusters_number()
 
-# User can set clusters and week number
-valid_value = 0
-n_clusters = 5
-while valid_value == 0:
-    n_clusters = int(input("Set number of clusters (from 2 to 10): "))
-    if 1 < n_clusters < 11:
-        valid_value = 1
-    else:
-        print("\nValue must be between [2;10]")
+scale_week_column()
 
-# Using KMeans
-kmeans = KMeans(n_clusters=n_clusters)
-y_predicted = kmeans.fit_predict(transactions[["W0", "Normalized 0"]])
-transactions["cluster"] = y_predicted
-transactions.head()
+# KMeans algorithm
+show_kmeans_chart('random', "KMeans")
 
-# Draw each cluster
-for index in range(n_clusters):
-    filtered_items = transactions[transactions.cluster == index]
-    plt.scatter(filtered_items["W0"], filtered_items["Normalized 0"], color=colors[index], marker=markers[index],
-                label="Skupienie " + str(index + 1))
+# KMeans++ algorithm
+show_kmeans_chart('k-means++', "KMeans++")
 
-# Draw center points and show legend
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], color='purple', marker='X', label="Centroidy")
-plt.title('KMeans - wykres z podziałem na klastry: %d' % n_clusters)
-plt.legend()
-plt.show()
-
-
-# DBSCAN - Create matrix from columns with analyzed data and use DBSCAN
-matrix = np.column_stack((transactions["W0"], transactions["Normalized 0"]))
-model = DBSCAN(eps=0.25, min_samples=400).fit(matrix)
-plt.scatter(matrix[:,0], matrix[:,1], c=model.labels_)
-plt.title('DBSCAN - wykres z podziałem na klastry: %d' % n_clusters)
-plt.show()
+# DBScan algorithm
+show_dbscan_chart()
